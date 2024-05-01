4bd39364ab5599139afca21d02fd73f8b1cfb296
Jesse Horner <jrhorner@pm.me>
2024-05-01 15:55:12 -0400

    add blink.c and update documentation

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # rpi5_lgpio_blink
-Basic LED blink program in C using libgpiod 2.1.1
+Basic LED blink program in C for the Raspberry Pi 5 using libgpiod 2.1.1. Raspberry Pi OS ships with version 1.6.3 which is not compatible with this code.  
+
+## Hardware setup
+
+ * Raspberry Pi 5
+ * 1 LED
+ * 1 1k Resistor
+ * Breadboard & Wires
+
+The LED cathode ( - , short leg) is connected to ground and the anode ( + , long leg) is connected to GPIO 21 through the resistor. Ref. [pinout](https://www.raspberrypi.com/documentation/computers/raspberry-pi.html)
+
+## Build
+
+```bash
+gcc -o blink blink.c -lgpiod
+```
+
+```
+jrhorner@raspberrypi5:~/blink $ gcc -o blink blink.c -lgpiod
+jrhorner@raspberrypi5:~/blink $ ./blink 
+Libgpiod version 2.1.1
+jrhorner@raspberrypi5:~/blink $ uname -a
+Linux raspberrypi5 6.6.28+rpt-rpi-2712 #1 SMP PREEMPT Debian 1:6.6.28-1+rpt1 (2024-04-22) aarch64 GNU/Linux
+```
+
+## libgpiod-2.1.1
+
+Build instructions for [libgpiod 2.1.1](https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git) on [Raspberry Pi OS x64 Lite](https://downloads.raspberrypi.com/raspios_lite_arm64/images/raspios_lite_arm64-2024-03-15/). 
+
+```bash
+sudo apt install autoconf autoconf-archive help2man libtool
+curl -O https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/snapshot/libgpiod-2.1.1.tar.gz
+tar xzf libgpiod-2.1.1.tar.gz
+cd libgpiod-2.1.1
+mkdir m4
+./autogen.sh --enable-tools=yes --prefix=/usr
+make
+sudo make install
+```
+
+Verify the version
+
+```bash
+gpiodetect -v
+```
+
+Optionally build the documentation
+
+```bash
+sudo apt install doxygen graphviz
+make doc
+```
---
+++ blink.c
@@ -0,0 +1,92 @@
+#include <gpiod.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define CHIP_PATH "/dev/gpiochip4"
+#define PIN   21
+
+int main(void) {
+    struct gpiod_chip *chip;
+    struct gpiod_line_settings *settings;
+    struct gpiod_line_request *request = NULL;
+    struct gpiod_line_config *line_conf;
+    struct gpiod_request_config *req_conf = NULL;
+    static const unsigned int offset = PIN;
+
+    printf("Libgpiod version %s\n", gpiod_api_version());\
+
+    // open the chip
+    chip = gpiod_chip_open(CHIP_PATH);
+	if (!chip)
+    {
+        printf("Failed to open chip");
+		return EXIT_FAILURE;
+    }
+
+    // create new line settings
+    settings = gpiod_line_settings_new();
+	if (!settings)
+		goto close_chip;
+
+    // set the GPIO direction as an output
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
+    // set the initial output value
+	gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_ACTIVE);
+
+    // create a new line config
+    line_conf = gpiod_line_config_new();
+	if (!line_conf)
+		goto free_settings;
+
+    // add the line settings to the line config
+    int ret = gpiod_line_config_add_line_settings(line_conf, &offset, 1, settings);
+	if (ret)
+		goto free_line_conf;
+
+    // create a new request config 
+    req_conf = gpiod_request_config_new();
+    if (!req_conf)
+        goto free_line_conf;
+
+    // set the consumer to the program name
+    gpiod_request_config_set_consumer(req_conf, "blink");
+
+    // make the request using the chip, request config, and line config
+	request = gpiod_chip_request_lines(chip, req_conf, line_conf);
+
+    // clear out the request config
+	gpiod_request_config_free(req_conf);
+
+free_line_conf:
+    // clear out the line config
+	gpiod_line_config_free(line_conf);
+
+free_settings:
+    // clear out the line settings
+	gpiod_line_settings_free(settings);
+
+close_chip:
+    // close the chip
+	gpiod_chip_close(chip);
+
+    if (!request) {
+		printf("failed to request line\n");
+		return EXIT_FAILURE;
+	}
+
+    // blink the led ten times
+    for (int i = 0; i < 10; i++) 
+    {   // 1 works in place of GPIOD_LINE_VALUE_ACTIVE
+        gpiod_line_request_set_value(request, offset, GPIOD_LINE_VALUE_ACTIVE); sleep(1);
+        sleep(1);
+        // 0 works in place of GPIOD_LINE_VALUE_INACTIVE
+        gpiod_line_request_set_value(request, offset, GPIOD_LINE_VALUE_INACTIVE); sleep(1);
+        sleep(1);
+    }
+
+    // release the GPIO request
+    gpiod_line_request_release(request);
+
+    return EXIT_SUCCESS;
+}
